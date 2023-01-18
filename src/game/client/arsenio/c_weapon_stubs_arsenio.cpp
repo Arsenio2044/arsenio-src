fea279980389041826af3f623bb366ab211639f9
Tuxxego <tux3go@gmail.com>
2023-01-18 15:39:12 -0500

    YEETED THE MINGUN FROM EARTH

--- src/game/client/arsenio/c_weapon_stubs_arsenio.cpp
+++ src/game/client/arsenio/c_weapon_stubs_arsenio.cpp
@@ -41,11 +41,11 @@ STUB_WEAPON_CLASS(weapon_crossbow, WeaponCrossbow, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_slam, Weapon_SLAM, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_crowbar, WeaponCrowbar, C_BaseHLBludgeonWeapon);
 STUB_WEAPON_CLASS(weapon_hopwire, WeaponHopwire, C_BaseHLCombatWeapon);
-//STUB_WEAPON_CLASS(weapon_bulk, WeaponBULK, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_bulk, WeaponBULK, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_mp5k, WeaponMP5K, C_HLSelectFireMachineGun ); // Gonna move to optux 3 at a later date. EDIT BY TUX: Gonna do this soon.
 STUB_WEAPON_CLASS(weapon_pro836, WeaponPRO836, C_HLSelectFireMachineGun);
 STUB_WEAPON_CLASS(weapon_gauss, WeaponGaussGun, C_BaseHLCombatWeapon);
-//STUB_WEAPON_CLASS(weapon_oicw, WeaponOICW, C_HLMachineGun);
+STUB_WEAPON_CLASS(weapon_oicw, WeaponOICW, C_HLMachineGun);
 STUB_WEAPON_CLASS(weapon_minigun, WeaponMiniGun, C_HLMachineGun);
 STUB_WEAPON_CLASS(weapon_addamm557, WeaponAdamm557, C_BaseHLCombatWeapon);
 STUB_WEAPON_CLASS(weapon_mobileturret, WeaponMobileTurret, C_BaseHLBludgeonWeapon);
@@ -60,6 +60,8 @@ STUB_WEAPON_CLASS( weapon_egon, WeaponEgon, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS(weapon_smg3, WeaponSMG3, C_HLSelectFireMachineGun);
 STUB_WEAPON_CLASS(weapon_kilo, WeaponKILO, C_HLSelectFireMachineGun);
 STUB_WEAPON_CLASS(weapon_50cal, Weapon50cal, C_BaseHLCombatWeapon);
+STUB_WEAPON_CLASS(weapon_minigun, WeaponMiniGun, C_HLMachineGun);
+// yeeted
 
 #endif
 
