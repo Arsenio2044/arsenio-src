518d62b0a4a9a2c2e39b3bb9d8bc0815c15b41ba
Tuxxego <tux3go@gmail.com>
2023-02-04 16:30:20 -0500

    Started working on overhauling an old UI system

---
+++ src/GameUI/panel_options.h
@@ -0,0 +1,63 @@
+//========= TuxUI ===================//
+//
+// Purpose: Replace GameUI with TuxUI.
+//
+//
+//=============================================================================//
+#pragma once
+
+#include "vgui2d/frame2d.h"
+#include "vgui_controls/AnimationController.h"
+
+#include "button_panel.h"
+
+class Panel_Options : public Frame2D
+{
+	DECLARE_CLASS_SIMPLE(Panel_Options, Frame2D);
+
+public:
+	Panel_Options(vgui::VPANEL Parent, const char* PanelName);
+
+	virtual void ApplySchemeSettings(vgui::IScheme* Scheme);
+	virtual void OnThink();
+	virtual void SetContentBounds();
+	virtual void Paint();
+	virtual void PaintBlurMask();
+	virtual void Animations();
+	virtual void DrawBackground();
+	virtual void DrawTitle();
+	virtual void DrawTabs();
+	virtual void DrawBasicButtons();
+	virtual void OnCommand(char const* Command);
+
+private:
+	vgui::AnimationController* AnimationController;
+
+	Button_Panel* ButtonApply;
+	Button_Panel* ButtonDone;
+	Button_Panel* ButtonBack;
+
+	int32 ContentX0;
+	int32 ContentY0;
+	int32 ContentX1;
+	int32 ContentY1;
+	int32 ContentW;
+	int32 ContentH;
+
+	wchar_t* PanelTitle;
+
+	int32 TitlePositionX;
+	int32 TitlePositionY;
+	int32 TitleSizeX;
+	int32 TitleSizeY;
+
+	Color BackgroundGradientTop;
+	Color BackgroundGradientBottom;
+
+	Color TitleColor;
+
+	float TitleOffsetX;
+	float TitleOffsetY;
+
+	vgui::HFont TitleFont;
+};
---
+++ src/GameUI/vgui2d/panel2d.h
@@ -0,0 +1,43 @@
+//========= TuxUI ===================//
+//
+// Purpose: Replace GameUI with TuxUI.
+//
+//
+//=============================================================================//
+#pragma once
+
+#include "vgui_controls/Panel.h"
+
+class Panel2D : public vgui::Panel
+{
+	DECLARE_CLASS_SIMPLE(Panel2D, vgui::Panel);
+
+public:
+	Panel2D();
+	Panel2D(vgui::Panel* Parent);
+	Panel2D(vgui::Panel* Parent, const char* PanelName);
+	Panel2D(vgui::Panel* Parent, const char* PanelName, vgui::HScheme Scheme);
+
+	virtual void Paint();
+	virtual void PaintBlurMask();
+	virtual void ApplySchemeSettings(vgui::IScheme* Scheme);
+
+	virtual bool IsBlur() const
+	{
+		return bBlurEnabled;
+	}
+
+	virtual bool IsFullyVisible()
+	{
+		return vgui::ipanel()->IsFullyVisible(GetVPanel());
+	}
+
+	virtual bool IsSchemeLoaded() const
+	{
+		return bSchemeLoaded;
+	}
+
+private:
+	bool bBlurEnabled;
+	bool bSchemeLoaded;
+};
