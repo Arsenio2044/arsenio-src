cf5ee5ac3ae7ba2ceb833609ab7cf7c995922c97
Tuxxego <tux3go@gmail.com>
2023-06-04 15:59:44 -0400

    Mouse input was already there lmfao

--- src/gamepadui/gamepadui_options.cpp
+++ src/gamepadui/gamepadui_options.cpp
@@ -415,15 +415,6 @@ public:
             UpdateConVar();
     }
 
-    void FireActionSignal()
-    {
-        BaseClass::FireActionSignal();
-
-        if (m_Options.Count())
-            m_nSelectedItem = (m_nSelectedItem + 1) % m_Options.Count();
-        if (m_bInstantApply)
-            UpdateConVar();
-    }
 
 
     void UpdateConVar() OVERRIDE
