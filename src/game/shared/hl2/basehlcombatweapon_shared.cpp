1d5eb6b84ef6cc51e73653195c94b4e89c31072a
Tuxxego <tux3go@gmail.com>
2023-07-31 19:06:34 -0400

    Viewmodel bob fix

--- src/game/shared/hl2/basehlcombatweapon_shared.cpp
+++ src/game/shared/hl2/basehlcombatweapon_shared.cpp
@@ -360,7 +360,7 @@ void CBaseHLCombatWeapon::WeaponIdle(void)
 float	g_lateralBob;
 float	g_verticalBob;
 
-#if defined( CLIENT_DLL ) && ( !defined( HL2MP ) && !defined( ASSMOD ) )
+#if defined( CLIENT_DLL ) && ( !defined( HL2MP ) && !defined( ARSENIO ) )
 
 #define	HL2_BOB_CYCLE_MIN	0.5f
 #define	HL2_BOB_CYCLE_MAX	0.45f
